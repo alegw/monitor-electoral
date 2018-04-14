0f60a29a077a8ef655bd4794cc106cea5d4024c2
Alejandro Machado <alejandro@macha.do>
2018-04-14 16:29:21 -0300

    Initial bot

--- src/bot.js
+++ src/bot.js
@@ -21,19 +21,19 @@ bot.onEvent = function(session, message) {
       onPayment(session, message)
       break
     case 'PaymentRequest':
-      welcome(session)
+      //welcome(session)
       break
   }
 }
 
 function onMessage(session, message) {
-  welcome(session)
+  sendMessage(session, `Recibimos tu mensaje o foto. Por favor, síguenos enviando más detalles y fotos sobre este incidente. Cuando quieras hablar sobre otro incidente, presiona el botón "Nuevo incidente".`)
 }
 
 function onCommand(session, command) {
   switch (command.content.value) {
-    case 'ping':
-      pong(session)
+    case 'nuevo_incidente':
+      nuevo_incidente(session)
       break
     case 'count':
       count(session)
@@ -69,11 +69,11 @@ function onPayment(session, message) {
 // STATES
 
 function welcome(session) {
-  sendMessage(session, `Hello Token!`)
+  sendMessage(session, `¡Hola! Somos el equipo de observación electoral para la jornada del 20 de mayo de 2018. Puedes reportar un incidente presionando el botón "Nuevo incidente" que ves abajo.\n\nApreciamos que nos envíes fotos de lo que está ocurriendo y tantos mensajes de texto como quieras detallando la situación.\n\nCuando quieras reportar otro incidente distinto, vuelve a presionar el botón "Nuevo incidente".`)
 }
 
-function pong(session) {
-  sendMessage(session, `Pong`)
+function nuevo_incidente(session) {
+  sendMessage(session, `Estás reportando un incidente. Mándanos fotos y relata lo que ocurre. Cuando quieras enviarnos detalles sobre otra ocurrencia distinta (incluso otra de la que ya hayas hablado antes), presiona el botón "Nuevo incidente".`)
 }
 
 // example of how to store state on each user
@@ -94,9 +94,9 @@ function donate(session) {
 
 function sendMessage(session, message) {
   let controls = [
-    {type: 'button', label: 'Ping', value: 'ping'},
-    {type: 'button', label: 'Count', value: 'count'},
-    {type: 'button', label: 'Donate', value: 'donate'}
+    {type: 'button', label: 'Nuevo incidente', value: 'nuevo_incidente'}
+    //,{type: 'button', label: 'Count', value: 'count'},
+    //{type: 'button', label: 'Donate', value: 'donate'}
   ]
   session.reply(SOFA.Message({
     body: message,
